8bc89190eae19ca2d6ca56dcdde9340e46e4bc1f
Cyril Vrousos <70748126+cyrilvrousos@users.noreply.github.com>
2023-08-03 10:53:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,3 +29,28 @@ html_theme = "furo"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+def prepare(app):
+    with open(DIR.parent.parent / "README.rst") as f:
+        contents = f.read()
+
+    if app.builder.name == "latex":
+        # Remove badges and stuff from start
+        contents = contents[contents.find(r".. start") :]
+
+        # Filter out section titles for index.rst for LaTeX
+        contents = re.sub(r"^(.*)\n[-~]{3,}$", r"**\1**", contents, flags=re.MULTILINE)
+
+    with open(DIR / "readme.rst", "w") as f:
+        f.write(contents)
+
+
+def clean_up(app, exception):  # noqa: ARG001
+    (DIR / "readme.rst").unlink()
+
+def setup(app):
+    # Copy the readme in
+    app.connect("builder-inited", prepare)
+
+    # Clean up the generated readme
+    app.connect("build-finished", clean_up)
