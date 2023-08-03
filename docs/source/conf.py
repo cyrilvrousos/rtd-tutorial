4b650e2be1bb1d83bb9ac8c3b46b43be98ced151
Cyril Vrousos <70748126+cyrilvrousos@users.noreply.github.com>
2023-08-03 10:32:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,10 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "breathe",
+    "sphinx_copybutton",
+    "sphinxcontrib.rsvgconverter",
+    "sphinxcontrib.moderncmakedomain",
 ]
 
 intersphinx_mapping = {
