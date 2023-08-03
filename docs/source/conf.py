fd22d20a513048037cc6ba911571e196f4798516
Cyril Vrousos <70748126+cyrilvrousos@users.noreply.github.com>
2023-08-03 10:42:33 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
+project = 'XSMP Modeler'
+copyright = '2023, Thales Alenia Space'
+author = 'Thales Alenia Space'
 
 # -- General configuration
 
